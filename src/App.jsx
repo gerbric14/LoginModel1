539e7e549a72e59f6cf561b3828c645678954a9e
German Briceno <gerbric14@gmail.com>
2023-08-04 01:13:37 -0400

    Login Model 1 Only Design

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Form from "./components/Form"
+
+
+function App() {
+
+
+  return (
+    <div className="flex w-full h-screen">
+      <div className="w-full flex items-center justify-center lg:w-1/2">
+        <Form />
+      </div>
+      <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center  bg-gray-200">
+        <div className="w-60 h-60 bg-gradient-to-tr from-violet-600 to-pink-600 rounded-full animate-spin" />
+        <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg" />
+
+      </div>
+    </div>
+  )
+}
+
+export default App
